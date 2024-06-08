fe63ccb9c5c07acc0450c4599fcc083726a34bc4
Bhat Owais <owaisbhat996@outlook.com>
2024-06-08 18:35:10 +0530

    Added options to search and clipboard commands

--- cmd/app/clipboard.go
+++ cmd/app/clipboard.go
@@ -18,21 +18,27 @@ var (
 type ClipboardCommand struct {
 	cmd      *flag.FlagSet
 	filePath *string
+	dir      *string
+	fileName *string
 }
 
 func NewClipboardCommand() ClipboardCommand {
 	clipboardCmd := flag.NewFlagSet("clipboard", flag.ExitOnError)
 	clipboardFilePath := clipboardCmd.String("filepath", "", "Text file path")
+	clipboardDir := clipboardCmd.String("dir", "./", "Directory")
+	clipboardFileName := clipboardCmd.String("filename", "", "Text file name")
 
 	return ClipboardCommand{
 		cmd:      clipboardCmd,
 		filePath: clipboardFilePath,
+		dir:      clipboardDir,
+		fileName: clipboardFileName,
 	}
 }
 
 func (command ClipboardCommand) execute() {
 	command.cmd.Parse(os.Args[2:])
-	if *command.filePath == "" {
+	if *command.filePath == "" && *command.fileName == "" {
 		command.cmd.Usage()
 		os.Exit(1)
 	}
@@ -45,7 +51,15 @@ func (command ClipboardCommand) execute() {
 }
 
 func (command ClipboardCommand) writeAll() error {
-	data, err := os.ReadFile(*command.filePath)
+	var file string
+
+	if *command.filePath != "" {
+		file = *command.filePath
+	} else {
+		file = *command.dir + "/" + *command.fileName
+	}
+
+	data, err := os.ReadFile(file)
 	if err != nil {
 		return err
 	}
--- cmd/app/fileSearch.go
+++ cmd/app/fileSearch.go
@@ -14,15 +14,18 @@ var ErrFileNotFound = errors.New("FILE NOT FOUND")
 type SearchCommand struct {
 	cmd      *flag.FlagSet
 	fileName *string
+	root     *string
 }
 
 func NewSearchCommand() SearchCommand {
 	searchCmd := flag.NewFlagSet("search", flag.ExitOnError)
 	searchFileName := searchCmd.String("filename", "", "Search file name")
+	rootDir := searchCmd.String("root", "/", "Root directory")
 
 	return SearchCommand{
 		cmd:      searchCmd,
 		fileName: searchFileName,
+		root:     rootDir,
 	}
 }
 
@@ -47,11 +50,9 @@ func (command SearchCommand) execute() {
 }
 
 func (command SearchCommand) searchFile() (string, error) {
-	root := "/"
-
 	var filePath string
 
-	err := filepath.Walk(root, func(path string, info fs.FileInfo, err error) error {
+	err := filepath.Walk(*command.root, func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
 			writeError("Error accessing", path, err)
 			return nil
